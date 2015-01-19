c39496fa1c6cd4df9f5652ded5db1011772871df
Markus Mayer <development@widemeadows.de>
2015-01-20 00:27:47 +0100

    added PddlTokenizer

---
+++ PDDL/Tokenizer/PddlTokenizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+using PDDL.Tokenizer.Tokens;
+
+namespace PDDL.Tokenizer
+{
+    /// <summary>
+    /// Class PddlTokenizer.
+    /// <para>
+    /// Implements a tokenizer for the PDDL 1.2 syntax.
+    /// </para>
+    /// </summary>
+    class PddlTokenizer
+    {
+        /// <summary>
+        /// Tokenizes the text using the given input stream reader.
+        /// </summary>
+        /// <param name="input">The input stream reder.</param>
+        /// <returns>A sequence of tokens.</returns>
+        [NotNull]
+        public IEnumerable<Token> Tokenize([NotNull] TextReader input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
