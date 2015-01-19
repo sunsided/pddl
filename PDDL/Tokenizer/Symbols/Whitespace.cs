619cdb8a9da9b4de9b559ab15755a758a53d1dfc
Markus Mayer <development@widemeadows.de>
2015-01-19 23:37:15 +0100

    added Whitespace symbol

---
+++ PDDL/Tokenizer/Symbols/Whitespace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PDDL.Tokenizer.Symbols
+{
+    /// <summary>
+    /// Class Whitespace.
+    /// </summary>
+    sealed class Whitespace : Symbol
+    {
+        /// <summary>
+        /// Gets the length.
+        /// </summary>
+        /// <value>The length.</value>
+        public int Length { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Whitespace" /> class.
+        /// </summary>
+        /// <param name="length">The length.</param>
+        /// <exception cref="System.ArgumentOutOfRangeException"><paramref name="length"/> must be positive</exception>
+        public Whitespace(int length)
+        {
+            if (length <= 0) throw new ArgumentOutOfRangeException("length", length, "length must be positive");
+            Length = length;
+        }
+    }
+}
