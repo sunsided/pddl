c876ca209455ad4a77dc2039a8dd64421894e38a
Markus Mayer <development@widemeadows.de>
2015-01-19 23:39:30 +0100

    added Comment symbol

---
+++ PDDL/Tokenizer/Symbols/Comment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace PDDL.Tokenizer.Symbols
+{
+    /// <summary>
+    /// Class Comment.
+    /// </summary>
+    sealed class Comment : Symbol
+    {
+        /// <summary>
+        /// Gets the comment.
+        /// </summary>
+        /// <value>The comment.</value>
+        [NotNull]
+        public string Value { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Comment" /> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> was <see langword="null"/></exception>
+        public Comment([NotNull] string value)
+        {
+            if (ReferenceEquals(value, null)) throw new ArgumentNullException("value", "value was null");
+            Value = value;
+        }
+    }
+}
