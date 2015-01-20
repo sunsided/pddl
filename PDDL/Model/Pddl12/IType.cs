b4225019f6b22f33b58dd5b1eb9c49349e007e86
Markus Mayer <development@widemeadows.de>
2015-01-20 19:57:51 +0100

    updated the types interfaces

--- PDDL/Model/Pddl12/IType.cs
+++ PDDL/Model/Pddl12/IType.cs
@@ -1,4 +1,6 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace PDDL.Model.Pddl12
 {
@@ -10,6 +12,51 @@ namespace PDDL.Model.Pddl12
     /// </para>
     /// </summary>
     public interface IType
+    {
+    }
+
+    /// <summary>
+    /// Interface IPlainType
+    /// <para>
+    ///     Describes a specific type within a <see cref="IDomain"/>,
+    ///     given the <c>:typing</c> requirement.
+    /// </para>
+    /// </summary>
+    public interface IPlainType : IType
+    {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        [NotNull]
+        string Name { get; }
+    }
+
+    /// <summary>
+    /// Interface IEitherType
+    /// <para>
+    ///     Describes a set of allowed types within a <see cref="IDomain"/>,
+    ///     given the <c>:typing</c> requirement.
+    /// </para>
+    /// </summary>
+    public interface IEitherType : IType
+    {
+        /// <summary>
+        /// Gets the options.
+        /// </summary>
+        /// <value>The options.</value>
+        [NotNull]
+        IReadOnlyList<IType> Types { get; }
+    }
+
+    /// <summary>
+    /// Interface IFluentType
+    /// <para>
+    ///     Describes a fluent type within a <see cref="IDomain"/>,
+    ///     given the <c>:typing</c> requirement.
+    /// </para>
+    /// </summary>
+    public interface IFluentType : IType
     {
         /// <summary>
         /// Gets the name.
