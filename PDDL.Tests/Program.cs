dcd1c6f11d260c086f4f8e637c616ee042f30c9d
Markus Mayer <development@widemeadows.de>
2015-01-20 02:05:52 +0100

    made unit test project a console application for faster debugging

---
+++ PDDL.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using PDDL.Tests.Tokenizer;
+
+namespace PDDL.Tests
+{
+    static class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        public static void Main()
+        {
+            var main = new PddlTokenizerTests();
+            main.FixtureSetUp();
+            main.TokenizationOfSimpleDomainDoesNotFail();
+        }
+    }
+}
