4393e728b496667712f5608683ad45dcc6c72f12
Markus Mayer <development@widemeadows.de>
2015-01-20 04:49:28 +0100

    added preconditions

---
+++ PDDL/Model/Pddl12/Null/NullGoalDescription.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using PDDL.Model.Pddl12.Goals;
+
+namespace PDDL.Model.Pddl12.Null
+{
+    /// <summary>
+    /// Class NullGoalDescription.
+    /// <para>
+    ///     Describes a nonexistant goal.
+    /// </para>
+    /// </summary>
+    public class NullGoalDescription : GoalBase
+    {
+        /// <summary>
+        /// The default instance
+        /// </summary>
+        [NotNull]
+        private static readonly NullGoalDescription _default = new NullGoalDescription();
+
+        /// <summary>
+        /// Returns the default instance of the <see cref="NullGoalDescription"/>
+        /// </summary>
+        /// <value>The default.</value>
+        [NotNull]
+        public static NullGoalDescription Default { get {  return _default; } }
+    }
+}
---
+++ PDDL/Model/Pddl12/Precondition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace PDDL.Model.Pddl12
+{
+    /// <summary>
+    /// Class Preconditions.
+    /// </summary>
+    public class Precondition : IPrecondition
+    {
+        /// <summary>
+        /// Gets the preconditions.
+        /// </summary>
+        /// <value>The preconditions.</value>
+        public IGoalDescription Preconditions { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Precondition"/> class.
+        /// </summary>
+        /// <param name="preconditions">The preconditions.</param>
+        /// <exception cref="ArgumentNullException">The value of 'preconditions' cannot be null. </exception>
+        public Precondition([NotNull] IGoalDescription preconditions)
+        {
+            if (ReferenceEquals(preconditions, null)) throw new ArgumentNullException("preconditions", "preconditions must not be null");
+            Preconditions = preconditions;
+        }
+    }
+}
