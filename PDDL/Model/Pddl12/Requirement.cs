5b9347d647e5e7cb1c644d9b35db6f5ee68aa112
Markus Mayer <development@widemeadows.de>
2015-01-20 05:22:03 +0100

    added requirement

---
+++ PDDL/Model/Pddl12/Requirement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace PDDL.Model.Pddl12
+{
+    /// <summary>
+    /// class Requirement
+    /// <para>
+    ///     Names a requirement for a <see cref="IDomain"/>.
+    /// </para>
+    /// </summary>
+    public class Requirement : IRequirement
+    {
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>The value.</value>
+        [NotNull]
+        public string Value { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Requirement"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ArgumentNullException">The value of 'value' cannot be null. </exception>
+        public Requirement([NotNull] string value)
+        {
+            if (ReferenceEquals(value, null)) throw new ArgumentNullException("value", "value must not be null");
+            Value = value;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Requirement" /> is equal to this instance.
+        /// </summary>
+        /// <param name="other">The object to compare with the current object.</param>
+        /// <returns><see langword="true" /> if the specified <see cref="Requirement" /> is equal to this instance; otherwise, <see langword="false" />.</returns>
+        protected bool Equals(Requirement other)
+        {
+            return string.Equals(Value, other.Value, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><see langword="true" /> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <see langword="false" />.</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Requirement) obj);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
+        public override int GetHashCode()
+        {
+            return Value.ToLowerInvariant().GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
