5a2b5571aa9a9eed02d1ea2398e7655b74be863b
Markus Mayer <development@widemeadows.de>
2015-01-20 03:09:32 +0100

    now also loading problem definitions for test

--- PDDL.Tests/Tokenizer/Pddl12TokenizerTests.cs
+++ PDDL.Tests/Tokenizer/Pddl12TokenizerTests.cs
@@ -22,7 +22,22 @@ namespace PDDL.Tests.Tokenizer
         /// The domain definition
         /// </summary>
         // ReSharper disable once NotNullMemberIsNotInitialized
-        [NotNull] private string _domainDefinition;
+        [NotNull] 
+        private string _domainDefinition;
+
+        /// <summary>
+        /// A problem definition
+        /// </summary>
+        // ReSharper disable once NotNullMemberIsNotInitialized
+        [NotNull]
+        private string _problem1;
+
+        /// <summary>
+        /// Another problem definition
+        /// </summary>
+        // ReSharper disable once NotNullMemberIsNotInitialized
+        [NotNull]
+        private string _problem2;
 
         /// <summary>
         /// Sets up the test fixture.
@@ -39,18 +54,44 @@ namespace PDDL.Tests.Tokenizer
         {
             var assembly = Assembly.GetExecutingAssembly();
             var resources = assembly.GetManifestResourceNames();
-            var domainFileName = resources.FirstOrDefault(name => name.Contains("DWR-operators.pddl"));
-            if (ReferenceEquals(domainFileName, null)) throw new InvalidOperationException("Require test data could not be found.");
+
+            {
+                var domainFileName = resources.FirstOrDefault(name => name.Contains("DWR-operators.pddl"));
+                if (ReferenceEquals(domainFileName, null)) throw new InvalidOperationException("Require test data could not be found.");
+                _domainDefinition = LoadNamedResourceString(assembly, domainFileName);
+                if (String.IsNullOrWhiteSpace(_domainDefinition)) Assert.Fail("Domain definition could not be loaded");
+            }
+
+            {
+                var problemFileName = resources.FirstOrDefault(name => name.Contains("DWR-pb1.pddl"));
+                if (ReferenceEquals(problemFileName, null)) throw new InvalidOperationException("Require test data could not be found.");
+                _problem1 = LoadNamedResourceString(assembly, problemFileName);
+                if (String.IsNullOrWhiteSpace(_problem1)) Assert.Fail("Problem definition could not be loaded");
+            }
+
+            {
+                var problemFileName = resources.FirstOrDefault(name => name.Contains("DWR-pb2.pddl"));
+                if (ReferenceEquals(problemFileName, null)) throw new InvalidOperationException("Require test data could not be found.");
+                _problem2 = LoadNamedResourceString(assembly, problemFileName);
+                if (String.IsNullOrWhiteSpace(_problem2)) Assert.Fail("Problem definition could not be loaded");
+            }
+        }
+
+        /// <summary>
+        /// Loads the named resource string.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <param name="domainFileName">Name of the domain file.</param>
+        private static string LoadNamedResourceString(Assembly assembly, string domainFileName)
+        {
             using (var stream = assembly.GetManifestResourceStream(domainFileName))
             {
                 Debug.Assert(stream != null, "stream != null");
                 using (var reader = new StreamReader(stream))
                 {
-                    _domainDefinition = reader.ReadToEnd();
+                    return reader.ReadToEnd();
                 }
             }
-
-            if (String.IsNullOrWhiteSpace(_domainDefinition)) Assert.Fail("Domain definition could not be loaded");
         }
 
         /// <summary>
@@ -62,18 +103,22 @@ namespace PDDL.Tests.Tokenizer
         {
             var tokenizer = new Pddl12Tokenizer();
 
-            using (var reader = new StringReader(_domainDefinition))
+            var definitions = new[] {_domainDefinition, _problem1, _problem2};
+            foreach (var definition in definitions)
             {
-                var tokens = tokenizer.Tokenize(reader);
-                Assert.NotNull(tokens, "tokens != null");
-
-                // dump the tokens
-                foreach (var token in tokens)
+                using (var reader = new StringReader(definition))
                 {
-                    if (token is PDDL.Tokenizer.Tokens.Whitespace) continue;
-                    Trace.WriteLine(token);
+                    var tokens = tokenizer.Tokenize(reader);
+                    Assert.NotNull(tokens, "tokens != null");
+
+                    // dump the tokens
+                    foreach (var token in tokens)
+                    {
+                        if (token is PDDL.Tokenizer.Tokens.Whitespace) continue;
+                        Trace.WriteLine(token);
+                    }
+                    Trace.Flush();
                 }
-                Trace.Flush();
             }
         }
     }
